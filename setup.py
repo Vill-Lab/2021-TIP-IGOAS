8cb9ab1fa1e0dc9f5720881a399ded9e0c21369d
Shuguang Dou <33367425+shuguang-52@users.noreply.github.com>
2021-12-29 17:50:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     description='A library for deep learning person re-identification in PyTorch',
     author='Kaiyang Zhou',
     license='MIT',
-    long_description=readme(),
+#     long_description=readme(),
     url='https://github.com/KaiyangZhou/deep-person-reid',
     packages=find_packages(),
     install_requires=get_requirements(),
